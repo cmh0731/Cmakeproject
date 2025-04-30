e1bf7df8e27a0feddcc4fd9c79e66a2e1a37f1bc
3A <alichen2001@qq.com>
2025-04-30 11:26:20 +0800

    version 0.0.2

---
+++ build/src/pnc_map/CMakeFiles/pnc_map.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pnc_map.dir/pnc_map.cpp.obj"
+  "CMakeFiles/pnc_map.dir/pnc_map.cpp.obj.d"
+  "E:/C++/Cmakeproject/bin/libpnc_map.dll"
+  "E:/C++/Cmakeproject/bin/libpnc_map.dll.manifest"
+  "E:/C++/Cmakeproject/bin/libpnc_map.pdb"
+  "libpnc_map.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pnc_map.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/process/CMakeFiles/process.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/process.dir/process.cpp.obj"
+  "CMakeFiles/process.dir/process.cpp.obj.d"
+  "E:/C++/Cmakeproject/bin/libprocess.dll"
+  "E:/C++/Cmakeproject/bin/libprocess.dll.manifest"
+  "E:/C++/Cmakeproject/bin/libprocess.pdb"
+  "libprocess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/process.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
